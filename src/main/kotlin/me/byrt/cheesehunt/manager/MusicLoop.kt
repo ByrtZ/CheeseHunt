09a4bfa03ff5d0c8c85a92d059337605a0e39bc8
ByrtZ <taylor.byrne417@gmail.com>
2023-03-16 17:19:56 +0000

    Remove music loop timer.

--- src/main/kotlin/me/byrt/cheesehunt/manager/MusicLoop.kt
+++ src/main/kotlin/me/byrt/cheesehunt/manager/MusicLoop.kt
@@ -1,9 +1,5 @@
 package me.byrt.cheesehunt.manager
 
-import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.format.NamedTextColor
-import net.kyori.adventure.text.format.TextDecoration
-
 import org.bukkit.entity.Player
 import org.bukkit.plugin.Plugin
 import org.bukkit.scheduler.BukkitRunnable
@@ -18,7 +14,6 @@ class MusicLoop(private val game : Game) {
         val bukkitRunnable = object: BukkitRunnable() {
             var musicTimer = 0
             override fun run() {
-                player.sendActionBar(Component.text("Music Loop Timer: ${String.format("%02d:%02d", musicTimer / 60, musicTimer % 60)}").color(NamedTextColor.GREEN).decoration(TextDecoration.BOLD, true))
                 if(musicTimer == 0) {
                     player.playSound(player.location, "event.downtime.loop", 1f, 1f)
                 }
